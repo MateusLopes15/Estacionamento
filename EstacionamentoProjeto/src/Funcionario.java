e194e13a3247435e0cb03b073727e3be7b65c9ab
MateusLopes15 <mateus1215.02@gmail.com>
2025-05-15 17:25:34 -0300

    git da tristeza

--- EstacionamentoProjeto/src/Funcionario.java
+++ EstacionamentoProjeto/src/Funcionario.java
@@ -1,25 +1,23 @@
 import java.time.LocalDateTime;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class Funcionario {
     Sistema sistema;
     Tarifa tarifa;
     Veiculo veiculo;
 
-    public boolean adicionarVeiculo(String placa, String modelo, String cor, String marca, Tipo tipo) {
+    
+
+    public boolean adicionarVeiculo(Veiculo veiculo) {
+        
         veiculo = new Veiculo();
+        
         for (int i = 0; i < sistema.veiculos.length; i++) {
-            if (sistema.veiculos[i] != null && sistema.veiculos[i].placa == placa) {
+            if (sistema.veiculos[i] != null && sistema.veiculos[i].placa == veiculo.placa) {
                 return false;
             }
 
             if (sistema.veiculos[i] == null) {
-                veiculo.placa = placa;
-                veiculo.modelo = modelo;
-                veiculo.cor = cor;
-                veiculo.marca = marca;
-                veiculo.tipo = tipo;
+                
                 sistema.veiculos[i]=veiculo;
             }
         }
@@ -30,16 +28,18 @@ public class Funcionario {
     public boolean removerVeiculoVaga(String placa, LocalDateTime horasaida) {
         return true;
     }
-
-    public boolean adicionarVeiculoVaga(String placa, LocalDateTime horaentrada) {
+    public boolean verificaExistenciaVeiculo(String placa){
+         for (int i = 0; i < sistema.veiculos.length; i++) {
+            if (sistema.veiculos[i] != null && sistema.veiculos[i].placa == veiculo.placa) {
+                return true;
+            }
+            
+        }
+        return false;
+    }
+    public boolean adicionarVeiculoVaga(String placa) {
         return true;
     }
 
-     boolean validaPlacaMercosul(String placa) {
-        Pattern pattern = Pattern.compile("^[a-zA-Z]{3}[0-9][A-Za-z0-9][0-9]{2}$");
-        Matcher matcher = pattern.matcher(placa); // Expressão regular para a validação de placas no formato Mercosul
-        
-        return matcher.matches(); // Retorna se a placa está na conformidade do formato Mercosul
-        
-    }
+     
 }
--- EstacionamentoProjeto/src/Sistema.java
+++ EstacionamentoProjeto/src/Sistema.java
@@ -7,4 +7,16 @@ public class Sistema {
 
     RegistroEstacionamento[] registros = new RegistroEstacionamento[10000];
     int numRegistros;
+
+
+public void listarVeiculos(){
+    for(int i =0; i<veiculos.length; i++){
+    System.out.println("Placa: " + this.veiculos[i].placa);
+    System.out.println("Modelo: " + this.veiculos[i].modelo);
+    System.out.println("Cor: " + this.veiculos[i].cor);
+    System.out.println("Marca: " + this.veiculos[i].marca);
+    System.out.println("Tipo: " + this.veiculos[i].tipo);
+    System.out.println("------------------");
+    }
+}
 }
